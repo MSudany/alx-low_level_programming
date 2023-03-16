4058e3b0abbb53957f54cebf8debca77b3a0fb02
MSudany <mssodany2002@gmail.com>
2023-03-16 23:38:46 +0000

    Fix 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-printf "with proper grammar, but the outcome is a piece of art,\n"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
