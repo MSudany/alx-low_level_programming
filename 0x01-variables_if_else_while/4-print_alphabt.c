3fc7f29e9e1dd4ebdbd41282e226da727a0bae78
MSudany <mssodany2002@gmail.com>
2023-03-19 04:25:51 +0000

    Add 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == 'e' || letter == 'q')
+			continue;
+		else
+			putchar (letter);
+	}
+	putchar ('\n');
+
+	return (0);
+}
