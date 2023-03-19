01f6a9b0aced535cd58c7876a6e066d18c4e3ec4
MSudany <mssodany2002@gmail.com>
2023-03-19 04:12:51 +0000

    Fix 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,12 +6,15 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
+	{
+		putchar (letter);
+	}
+	putchar ('\n');
 
 	return (0);
 }
