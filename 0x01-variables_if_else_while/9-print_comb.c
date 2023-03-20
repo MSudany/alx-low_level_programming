22193024d1165769bbcbfd8244105ed11bfd8041
MSudany <mssodany2002@gmail.com>
2023-03-20 02:35:43 +0000

    Fix 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,13 +11,15 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i < 57; i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
+		if (i == 57)
+			continue;
 		putchar(',');
 		putchar(' ');
 	}
-	putchar('9');
+	putchar('\n');
 
 	return (0);
 }
