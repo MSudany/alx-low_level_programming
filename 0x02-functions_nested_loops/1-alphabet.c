89886ada75c7b5943661aed9293411f43e2859af
MSudany <mssodany2002@gmail.com>
2023-03-23 00:52:41 +0000

    Fix 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,15 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "main.h"
 
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+void print_alphabet(void)
+{
+        int i;
+
+        for (i = 97; i < 123; i++)
+                _putchar(i);
+        _putchar('\n');
+
+        return;
+}
