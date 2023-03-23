22883d2b7aa730d0f7149846c088aee8dc9a6518
MSudany <mssodany2002@gmail.com>
2023-03-23 00:36:18 +0000

    Fix 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <string.h>
 #include "main.h"
 
 /**
@@ -10,9 +8,3 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-	print_alphabet();
-
-	return (0);
-}
