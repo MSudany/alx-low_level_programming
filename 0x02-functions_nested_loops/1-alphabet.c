671b7d897cdd6cbabacead6e0b846f69a5268313
MSudany <mssodany2002@gmail.com>
2023-03-23 00:11:46 +0000

    Add 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+}
