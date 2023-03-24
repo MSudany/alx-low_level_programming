4593f3843bdea92851887cc09c5f12da9abb7424
MSudany <mssodany2002@gmail.com>
2023-03-24 05:17:37 +0000

    Fix  & Add square, triangle, fizzbuzz

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: triangle size
+ *
+ * Description: prints a triangle
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size - i; j++)
+				_putchar(' ');
+			for (j = 0; j <= i; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
