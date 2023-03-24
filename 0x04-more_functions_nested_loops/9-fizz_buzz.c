2cb2ac6c0a6acda70e71b57e5020cec7356b0f55
MSudany <mssodany2002@gmail.com>
2023-03-24 05:36:39 +0000

    Fix 10-print_triangle.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,13 +12,14 @@ int main(void)
 	for (i = 1; i < 101; i++)
 	{
 		if (i % 3 != 0 && i % 5 != 0)
-			printf("%d ", i);
+			printf("%d", i);
 		else if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (i % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else
-			printf("Buzz ");
+			printf("Buzz");
+		printf(" ");
 	}
 	printf("\n");
 
