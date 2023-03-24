5497166271dc3229f05ce677636e79f80036ad69
MSudany <mssodany2002@gmail.com>
2023-03-24 01:08:37 +0000

    Fix 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints most numbers from 0 to 9
+ * 
+ * Description: prints most numbers from 0 to 9
+ * Return: void.
+ */
+void print_most_numbers(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		if (i == 50 || i == 52)
+			continue;
+		else
+			_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}	
