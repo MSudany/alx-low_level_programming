3bbf1ad148e9b5660119af0a06ad628c483c1b8a
msudany <mssodany2002@gmail.com>
2023-04-10 16:18:52 +0000

    Add 1-argc.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: args counter
+ * @argv: args container
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void) argv;
+	printf("%d\n", argc);
+
+	return (0);
+}
