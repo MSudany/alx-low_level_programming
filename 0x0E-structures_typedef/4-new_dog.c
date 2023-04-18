8f1877e82079411da55499f995d91ee16d5d02dc
msudany <mssodany2002@gmail.com>
2023-04-18 01:38:50 +0000

    Fix 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,8 @@
 #include "dog.h"
 #include <stdlib.h>
+
+int _strlen(char *s);
+
 /**
  * new_dog - creates a new struct dog
  * @name: dog's name
@@ -11,14 +14,45 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d = malloc(sizeof(d));
+	dog_t *d;
+	int n, o;
 
-	if (d == NULL)
-		return (NULL);
+	if (name != NULL && owner != NULL)
+	{	
+		n = _strlen(name) + 1;
+		o = _strlen(owner) + 1;
 
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+		d = malloc(sizeof(dog_t));
+		if (d == NULL)
+			return (NULL);
+		
+		d->name = malloc(sizeof(n));
+		if(d->name == NULL)
+		{
+			free(d);
+			return (NULL);
+		}
+
+		d->owner = malloc(sizeof(o));
+		if(d->owner == NULL)
+		{
+			free(d->name);
+			free(d);
+			return (NULL);
+		}
+
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 
 	return (d);
+}
+
+int _strlen(char *s)
+{
+	int i;
+	for (i = 0; *s != '\0'; s++)
+		i++;
+	return (i);
 }
