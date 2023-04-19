1572ee1f5237ba64f0fe7c5fa3be028b94a2829e
msudany <mssodany2002@gmail.com>
2023-04-19 23:11:34 +0000

    Fix simple operations files

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -24,9 +24,10 @@ int (*get_op_func(char *s))(int, int)
 	{
 		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
+
 		i++;
 	}
 
-	return (0);
+	return (NULL);
 }
 
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,14 +12,26 @@
 int main(int argc, char *argv[])
 {
 	int a, b;
+	int (*o)(int, int);
 
 	if (argc != 4)
-		return (0);
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
-	printf("%d\n", get_op_func(argv[2])(a, b));
+	o = get_op_func(argv[2]);
+
+	if (!o)
+	{
+		printf("Error\n");
+		exit (99);
+	}
+
+	printf("%d\n", o(a, b));
 
 	return (0);
 }
