d79193c2b746f17546661cf208c9b31c06471416
MSudany <mssodany2002@gmail.com>
2023-05-09 17:39:36 +0300

    Fix 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,13 +20,13 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (f == -1)
-		return (-1);
-
 	if (s == NULL)
 		return (-1);
 
 	f = open(filename, O_RDWR | O_CREAT);
+
+	if (f == -1)
+		return (-1);
 
 	if (text_content == NULL)
 	{
