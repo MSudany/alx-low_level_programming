8b251593cae733f8d7b25964757cb0a349978cdc
MSudany <mssodany2002@gmail.com>
2023-05-09 18:04:13 +0300

    Fix 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,8 +1,10 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
-#include <string.h>
+#include <sys/stat.h>
 #include "main.h"
+
+int _strlen(char *s);
 
 /**
  * create_file - ...
@@ -15,7 +17,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int f, i;
-	char *s = malloc(sizeof(char *) * strlen(text_content));
+	char *s = malloc(sizeof(char *) * _strlen(text_content));
 
 	if (filename == NULL)
 		return (-1);
@@ -23,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 	if (s == NULL)
 		return (-1);
 
-	f = open(filename, O_RDWR | O_CREAT);
+	f = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (f == -1)
 		return (-1);
@@ -35,10 +37,26 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 
-	i = read(f, s, strlen(text_content));
+	i = read(f, s, _strlen(text_content));
 	write(f, s, i);
 
 	free(s);
 	close(f);
 	return (1);
+}
+
+/**
+  * _strlen - ...
+  * @s: ...
+  *
+  * Return: ...
+  */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	return (i);
 }
