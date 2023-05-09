76705eb619ab3b7ce5e963aa9d48cd3654140490
MSudany <mssodany2002@gmail.com>
2023-05-09 16:09:21 +0300

    Add 0-read_textfile.c, Modify main.h

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
+
+/**
+ * read_textfile - ...
+ * @filename: ...
+ * @letters: ...
+ * 
+ * Description: ...
+ * Return: ...
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int f, _read;
+	char *buff = malloc(sizeof(char *) * letters);
+
+	if (buff == NULL)
+		return (0);
+
+	if (filename == NULL)
+		return (0);
+
+	f = open(filename, O_RDWR);
+	if (f == -1)
+		return (0);
+
+	_read = read(f, buff, letters);
+	if (_read == -1)
+		return (0);
+
+	write(f, buff, _read);
+	
+	free(buff);
+	close(f);
+	return (_read);
+}
