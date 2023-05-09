2eaeac46e32fe8c63c3e78b401a9786be29b414d
MSudany <mssodany2002@gmail.com>
2023-05-09 17:24:56 +0300

    Add 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * create_file - ...
+ * @filename: ...
+ * @text_content: ...
+ *
+ * Description: ...
+ * Return: ...
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int f = open(filename, O_RDWR | O_CREAT), s = malloc(sizeof(char *) * strlen(text_content));
+
+	if (f == -1)
+		return (0);
+
+	if (s == NULL)
+		return (0);
+
+	write(f, s, strlen(text_content));
+
+	free(s);
+	close(f);
+	return (0);
+}
