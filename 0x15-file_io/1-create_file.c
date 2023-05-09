32fab9adcaf2b2f9c37678a28f2edfb00d866352
MSudany <mssodany2002@gmail.com>
2023-05-09 17:26:12 +0300

    Fix 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int f = open(filename, O_RDWR | O_CREAT), s = malloc(sizeof(char *) * strlen(text_content));
+	int f = open(filename, O_RDWR | O_CREAT), *s = malloc(sizeof(char *) * strlen(text_content));
 
 	if (f == -1)
 		return (0);
