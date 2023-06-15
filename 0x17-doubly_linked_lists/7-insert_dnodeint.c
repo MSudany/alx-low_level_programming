f35d7e42ece51e3f3142a248003b48509f5685bb
msudany <mssodany2002@gmail.com>
2023-06-16 00:12:28 +0300

    Update 7.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,21 +21,29 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new_t->n = n;
 		node_t = *h;
 
-		if (*h == NULL)
+		if (idx == 0)
 		{
-			new_t->next = NULL;
-			new_t->prev = NULL;
+			new_t->next = node_t;
+			new_t->prev = node_t->prev;
+			node_t->prev = new_t;
 			*h = new_t;
 			return (new_t);
 		}
 
-		while (node_t->next && i <= idx)
+		while (node_t && i <= idx)
 		{
-			if (i == idx)
+			if (node_t->next == NULL && i == idx)
 			{
 				new_t->next = node_t->next;
 				new_t->prev = node_t;
-				node_t->next = new_t;
+				return (new_t);
+			}
+
+			if (i == idx)
+			{
+				new_t->next = node_t->prev->next;
+				new_t->prev = node_t->prev;
+				node_t->prev->next = new_t;
 				return (new_t);
 			}
 			node_t = node_t->next;
