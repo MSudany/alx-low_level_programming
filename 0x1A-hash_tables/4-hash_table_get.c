33575858bb57a64b32433716e5a6c075c9acd610
msudany <mssodany2002@gmail.com>
2023-07-07 18:13:24 +0300

    Add 4.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - returns the value associated with a key
+ * @ht: hash table
+ * @key: key to look for
+ *
+ * Return: value associated with the key or NULL on failure
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+
+	if (ht && key && ht->array && ht->size)
+	{
+		index = key_index((const unsigned char *)key, ht->size);
+		if (index < ht->size)
+			return (ht->array[index]->value);
+		return (NULL);
+	}
+	return (NULL);
+}
+
